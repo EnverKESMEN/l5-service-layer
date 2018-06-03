997628cfae03b438185923d2b614a4821568ca7c
Enver KESMEN <kesmenenver@gmail.com>
2018-06-04 00:27:01 +0300

    Add service provider and laravel auto-discover settings

---
+++ service-layer/src/ServiceLayerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace KesmenEnver\ServiceLayer;
+
+use Illuminate\Support\ServiceProvider;
+
+class ServiceLayerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
